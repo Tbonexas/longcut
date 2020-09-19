2f6c830ca991f4920873b593285004c58158cbda
Giovanny Franco <gfranco19@live.com>
2020-09-19 12:39:10 -0700

    added marker

--- assets/js/mapscript.js
+++ assets/js/mapscript.js
@@ -1,41 +1,39 @@
-// Global variables // 
-var infoWindow;
+     var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 15
+        });
+        infoWindow = new google.maps.InfoWindow;
 
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+    var marker = new google.maps.Marker({
+      position: pos,
+      map: map,
+      title: 'Hello World!'
+    });
+  
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
 
-// call google maps API //
-function initMap() {
-    var options = {
-// options for map starting point //         
-    zoom:15,
-    // Riverside starting location // 
-    center:{lat:33.9806, lng:-117.3755}
-}
-// create map object // 
-var map = new google.maps.Map(document.getElementById('map'), options);
-infoWindow = new google.maps.InfoWindow;
-//current location // 
-if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position){
-// variable for postion //
-var pos = {
-    lat: position.coords.latitude,
-    lng: position.coords.longitude
-};
-infoWindow.setPosition(pos);
-infoWindow.setContent('Location Found.');
-infoWindow.open(map);
-map.setContent(pos);
-    })
-}else{
-    // if no geolocation is supported on broswer //
-    handleLocationError(false, infoWindow, map.getCenter());
-   }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ? 
-                            'Error: The Geolocation Service Failed.' :
-                            'Error: Your browser does not support geolocation');
-    infoWindow.open(map);
-}
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
