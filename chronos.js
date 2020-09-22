05708fa5bca5f747cf453fb4d1adbf80fc04e74a
Jorge Martinez <j.a.mjorgemartinez21@gmail.com>
2020-09-21 18:57:01 -0700

    added more buttons and a chart

---
+++ chronos.js
@@ -0,0 +1,16 @@
+function statsRedirect() {
+    window.location.href = "file:///C:/Users/jamjo/Documents/Project-1/longcut/navigate.html";
+    
+    
+
+} 
+
+  function recordsRedirect() {
+    window.location.href = "file:///C:/Users/jamjo/Documents/Project-1/longcut/navigate.html";
+  } 
+
+
+  function accomplishRedirect() {
+    window.location.href = "file:///C:/Users/jamjo/Documents/Project-1/longcut/navigate.html";
+  } 
+
